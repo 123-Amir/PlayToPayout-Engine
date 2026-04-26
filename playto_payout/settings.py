028e2d2c5bcd3f1376471c8d3a6d4bce9135bda0
Amir Hussain <140737542+123-Amir@users.noreply.github.com>
2026-04-26 17:06:55 +0530

    Update settings.py

--- playto_payout/settings.py
+++ playto_payout/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'False'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1','playtopayout-engine-2.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
